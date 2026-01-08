73cf5156e2684b8fa9b34067441a8b4b6bc5fee7
AymericLeFeyer <lefeyer.aymeric@gmail.com>
2026-01-08 16:52:41 +0100

    chore(app): update docker compose

--- src/features/domadoo/datasource.js
+++ src/features/domadoo/datasource.js
@@ -6,7 +6,7 @@ exports.fetchDomadooAffiliateData = async () => {
 
     return await axios.get(url, {
         headers: {
-            'Cookie': cookie,
+            'Cookie': cookie.replace(/\$\$/g, '$'),
             'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/',
             'Accept': 'text/html',
         },
