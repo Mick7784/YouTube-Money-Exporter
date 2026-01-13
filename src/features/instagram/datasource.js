0e750e5f2ab672da7244c8d7ae3c0f6a5d1336b2
AymericLeFeyer <lefeyer.aymeric@gmail.com>
2026-01-13 13:51:14 +0100

    feat(discord): add server stats

--- src/features/instagram/datasource.js
+++ src/features/instagram/datasource.js
@@ -1,14 +1,19 @@
-const axios = require('axios');
+const axios = require("axios");
 
 exports.fetchInstagramProfile = async () => {
+  try {
     const username = process.env.INSTAGRAM_USERNAME;
     const url = `https://i.instagram.com/api/v1/users/web_profile_info/?username=${username}`;
     const response = await axios.get(url, {
-        headers: {
-            'User-Agent': 'Instagram 76.0.0.15.395 Android (24/7.0; 640dpi; 1440x2560; samsung; SM-G930F; herolte; samsungexynos8890; en_US; 138226743)',
-            'Accept': 'application/json',
-        }
-    }); 
+      headers: {
+        "User-Agent":
+          "Instagram 76.0.0.15.395 Android (24/7.0; 640dpi; 1440x2560; samsung; SM-G930F; herolte; samsungexynos8890; en_US; 138226743)",
+        Accept: "application/json",
+      },
+    });
 
     return response.data;
-}
+  } catch (error) {
+    console.error("Error fetching Instagram profile:", error);
+  }
+};
--- src/features/instagram/service.js
+++ src/features/instagram/service.js
@@ -1,18 +1,23 @@
-const datasource = require('./datasource');
-const cache = require('../../utils/cache');
+const datasource = require("./datasource");
+const cache = require("../../utils/cache");
 
 exports.fetchInstagramData = async () => {
+  try {
     const data = (await datasource.fetchInstagramProfile()).data;
 
     const result = {
-        username: data.user.username,
-        followers: data.user.edge_followed_by.count,
-        following: data.user.edge_follow.count,
-        posts: data.user.edge_owner_to_timeline_media.count,
-        lastUpdate: new Date().toISOString(),
-    }
+      username: data.user.username,
+      followers: data.user.edge_followed_by.count,
+      following: data.user.edge_follow.count,
+      posts: data.user.edge_owner_to_timeline_media.count,
+      lastUpdate: new Date().toISOString(),
+    };
 
-    cache.set(result, 'instagram.json');
+    cache.set(result, "instagram.json");
 
-    return result
-}
+    return result;
+  } catch (error) {
+    console.error("Error in fetchInstagramData:", error);
+    return cache.get("instagram.json");
+  }
+};
